f48236c78270191c81e90224c260e9abf1ce4a2c
Pavlichek <gaevski321@icloud.com>
2020-09-11 18:54:47 -0400

    upgrade

--- test_main_page.py
+++ test_main_page.py
@@ -4,6 +4,6 @@ def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page1(browser):
+def test_guest_can_go_to_login_page(browser):
    browser.get(link)
    go_to_login_page(browser)
