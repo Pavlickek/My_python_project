2b8dd968b1b413c740b96d90f2f5f5f1aa095d72
Pavlichek <gaevski321@icloud.com>
2020-09-12 00:47:15 -0400

    def test_forms_registration_login(browser):

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,9 @@
 from .page.main_page import MainPage
-
+from .page.Login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, link)   # инициализируем Page Object(класс MainPage), передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()
 
@@ -12,3 +12,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_forms_registration_login(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+
