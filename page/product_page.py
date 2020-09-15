7e9e43e16545185487dcc42c73388fb1fc2422bd
Pavlichek <gaevski321@icloud.com>
2020-09-14 22:32:48 -0400

    upgrade test product page

--- page/product_page.py
+++ page/product_page.py
@@ -21,25 +21,29 @@ class ProductPage(BasePage):
           self.coast_of_basket_equal_coast_of_book()
 
       def message_book_add_in_basket(self):
-          assert self.browser.find_element(*BasketLocators.ADD_BOOK)
+          assert self.is_element_present(*BasketLocators.ADD_BOOK), ("Message about adding is not presented")
           assert True
 
 
       def massage_of_book_should_be_equal_picked_book(self):
+          assert self.is_element_present(*BasketLocators.NAME_BOOK_IN_MESSAGE), ("Name of book in message didnt found")
+          assert self.is_element_present(*BasketLocators.NAME_BOOK_IN_BASKET), ("Name of book in basket didnt found")
           NAME_BOOK_IN_MESSAGE = self.browser.find_element(*BasketLocators.NAME_BOOK_IN_MESSAGE).text
           NAME_BOOK_IN_BASKET = self.browser.find_element(*BasketLocators.NAME_BOOK_IN_BASKET).text
-          assert NAME_BOOK_IN_MESSAGE == NAME_BOOK_IN_BASKET
+          assert NAME_BOOK_IN_MESSAGE == NAME_BOOK_IN_BASKET, ("Name of book in message not equal name of book in basket")
           assert True
 
 
       def massage_of_coast_of_basket(self):
-          assert self.browser.find_element(*BasketLocators.MESS_COAST_BASKET)
+          assert self.is_element_present(*BasketLocators.MESS_COAST_BASKET), ("Message about coast of basket is not presented")
           assert True
 
 
 
       def coast_of_basket_equal_coast_of_book(self):
+           assert self.is_element_present(*BasketLocators.COAST_BASKET), ("Coast of book in basket didnt found")
+           assert self.is_element_present(*BasketLocators.COAT_BOOK), ("Coast of book  didnt found")
            COAST_OF_BASKET = self.browser.find_element(*BasketLocators.COAST_BASKET).text
            COAST_OF_BOOK = self.browser.find_element(*BasketLocators.COAT_BOOK).text
-           assert   COAST_OF_BASKET == COAST_OF_BOOK
+           assert COAST_OF_BASKET == COAST_OF_BOOK, ("Coast of book  not equal coast of book in basket")
            assert True
