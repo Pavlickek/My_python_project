65772d0796dc43f34723a85cd24665d9bf5a1948
Pavlichek <gaevski321@icloud.com>
2020-09-17 21:23:03 -0400

    def test_guest_should_see_login_link_on_product_page(browser): def test_guest_can_go_to_login_page_from_product_page(browser):

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,25 @@
 from .page.product_page import ProductPage
+from .page.Login_page import LoginPage
 import pytest
+
+
+@pytest.mark.smoke
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+
+@pytest.mark.smoke
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_page()
+
 
 @pytest.mark.parametrize('links', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -21,3 +41,4 @@ def test_user_can_add_book_in_basket(browser, links):
     page.open()
     page.add_books_in_basket()
     page.should_be_in_basket()
+
