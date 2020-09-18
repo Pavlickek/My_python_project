c119606349f4bd3daaae060929b0a701b26cff1e
Pavlichek <gaevski321@icloud.com>
2020-09-18 00:39:56 -0400

    add new test for testing of basket from main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,24 @@
 from .page.main_page import MainPage
 from .page.Login_page import LoginPage
+from .page.basket_page import BasketPage
+import pytest
+
+
+@pytest.mark.smoke
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_the_basket()
+    page.basket_is_empty()
+    page.string_about_basket_is_empty()
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object(класс MainPage), передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
+    page = MainPage(browser,
+                    link)  # инициализируем Page Object(класс MainPage), передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
     page.go_to_login_page()
 
 
@@ -21,5 +34,3 @@ def test_forms_registration_login(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
-
-
